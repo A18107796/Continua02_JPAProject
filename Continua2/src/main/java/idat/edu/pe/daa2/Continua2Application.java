bb6d33827874ac4765b36d0d9c8b803b3dc5748a
the_n <the_n@192.168.1.60>
2019-11-18 15:34:19 -0500

    Original.

---
+++ Continua2/src/main/java/idat/edu/pe/daa2/Continua2Application.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Continua2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Continua2Application.class, args);
+	}
+
+}
---
+++ Continua2/src/main/java/idat/edu/pe/daa2/modelo/Alumno.java
@@ -0,0 +1,17 @@
+package idat.edu.pe.daa2.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno implements Serializable{
+
+	
+	
+	
+	private static final long serialVersionUID = 1L;
+
+}
